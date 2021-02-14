145287c062ac54c36814346be63e126a2e98071b
Kunal Bhatti <kunal.bhatti15@gmail.com>
2021-02-14 19:41:07 +0530

    corrected app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const authController = require('./controllers/auth.controller');
 const socialAuthController = require('./controllers/social-auth.controller');
